99458113ba733decf5769b3d5f1ef3afa378a67b
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-10-08 12:22:05 -0500

    Update database.js

--- database.js
+++ database.js
@@ -1,113 +1,45 @@
 var SongNames = [{
+  "title": "Mania",
+  "artist": "Fall Out Boy",
+  "artwork": "artwork",
+  "tracks": [{
+    "title": "song title", "source": "test.mp3"	
+   }, {
+    "title": "song title", "source": "test.mp3"	
+   }, {
+    "title": "song title", "source": "test.mp3"	
+  }]
+}, {
   "title": "title",
-	"artist": "artist",
-	"artwork": "artwork",
-	"tracks": [{
-		"title": "song title",
-		"source": "test.mp3"	
-	}, {
-		"title": "song title",
-		"source": "test.mp3"	
-	}, {
-		"title": "song title",
-		"source": "test.mp3"	
-	}]
+  "artist": "artist",
+  "artwork": "artwork",
+  "tracks": [{
+    "title": "song title", "source": "test.mp3"	
+   }, {
+    "title": "song title", "source": "test.mp3"	
+   }, {
+    "title": "song title", "source": "test.mp3"	
+  }]
 }, {
-	"title": "title",
-	"artist": "artist",
-	"artwork": "artwork",
-	"tracks": [{
-		"title": "song title",
-		"source": "test.mp3"	
-	}, {
-		"title": "song title",
-		"source": "test.mp3"	
-	}, {
-		"title": "song title",
-		"source": "test.mp3"	
-	}]
+  "title": "title",
+  "artist": "artist",
+  "artwork": "artwork",
+  "tracks": [{
+    "title": "song title", "source": "test.mp3"	
+   }, {
+    "title": "song title", "source": "test.mp3"	
+   }, {
+    "title": "song title", "source": "test.mp3"	
+  }]
 }, {
-	"title": "title",
-	"artist": "artist",
-	"artwork": "artwork",
-	"tracks": [{
-		"title": "song title",
-		"source": "test.mp3"	
-}, {
-		"title": "song title",
-		"source": "test.mp3"	
-}, {
-		"title": "song title",
-		"source": "test.mp3"	
-	}]
-}, {
-	"title": "title",
-	"artist": "artist",
-	"artwork": "artwork",
-	"tracks": [{
-		"title": "song title",
-		"source": "test.mp3"	
-	}, {
-		"title": "song title",
-		"source": "test.mp3"	
-	}, {
-		"title": "song title",
-		"source": "test.mp3"	
-	}]
-}, {
-	"title": "title",
-	"artist": "artist",
-	"artwork": "artwork",
-	"tracks": [{
-		"title": "song title",
-		"source": "test.mp3"	
-	}, {
-		"title": "song title",
-		"source": "test.mp3"	
-	}, {
-		"title": "song title",
-		"source": "test.mp3"	
-	}]
-}, {
-	"title": "title",
-	"artist": "artist",
-	"artwork": "artwork",
-	"tracks": [{
-		"title": "song title",
-		"source": "test.mp3"	
-	}, {
-		"title": "song title",
-		"source": "test.mp3"	
-	}, {
-		"title": "song title",
-		"source": "test.mp3"	
-	}]
-}, {
-	"title": "title",
-	"artist": "artist",
-	"artwork": "artwork",
-	"tracks": [{
-		"title": "song title",
-		"source": "test.mp3"	
-	}, {
-		"title": "song title",
-		"source": "test.mp3"	
-	}, {
-		"title": "song title",
-		"source": "test.mp3"	
-	}]
-}, {
-	"title": "title",
-	"artist": "artist",
-	"artwork": "artwork",
-	"tracks": [{
-		"title": "song title",
-		"source": "test.mp3"	
-	}, {
-		"title": "song title",
-		"source": "test.mp3"	
-	}, {
-		"title": "song title",
-		"source": "test.mp3"		
-	}]
+  "title": "title",
+  "artist": "artist",
+  "artwork": "artwork",
+  "tracks": [{
+    "title": "song title", "source": "test.mp3"	
+   }, {
+    "title": "song title", "source": "test.mp3"	
+   }, {
+    "title": "song title", "source": "test.mp3"	
+  }]
 }];
