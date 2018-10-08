d270dc3f89aadb65a752c9dcb160e7177b167cfe
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-10-08 12:05:02 -0500

    Create database.js

---
+++ database.js
@@ -0,0 +1,30 @@
+var SongNames = [{
+  "title": "title",
+	"artist": "artist",
+	"artwork": "artwork",
+	"tracks": [{
+		"title": "song title",
+		"source": "test.mp3"	
+	}, {
+		"title": "song title",
+		"source": "test.mp3"	
+	}, {
+		"title": "song title",
+		"source": "test.mp3"	
+	}]
+},
+{
+	"title": "title",
+	"artist": "artist",
+	"artwork": "artwork",
+	"tracks": [{
+		"title": "song title",
+		"source": "test.mp3"	
+	}, {
+		"title": "song title",
+		"source": "test.mp3"	
+	}, {
+		"title": "song title",
+		"source": "test.mp3"		
+	}]
+}];
