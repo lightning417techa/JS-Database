788b963d4108c8e8465c24ed2922b235306833ce
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-10-08 12:14:05 -0500

    Update database.js

--- database.js
+++ database.js
@@ -12,8 +12,91 @@ var SongNames = [{
 		"title": "song title",
 		"source": "test.mp3"	
 	}]
-},
-{
+}, {
+	"title": "title",
+	"artist": "artist",
+	"artwork": "artwork",
+	"tracks": [{
+		"title": "song title",
+		"source": "test.mp3"	
+	}, {
+		"title": "song title",
+		"source": "test.mp3"	
+	}, {
+		"title": "song title",
+		"source": "test.mp3"	
+	}]
+}, {
+	"title": "title",
+	"artist": "artist",
+	"artwork": "artwork",
+	"tracks": [{
+		"title": "song title",
+		"source": "test.mp3"	
+}, {
+		"title": "song title",
+		"source": "test.mp3"	
+}, {
+		"title": "song title",
+		"source": "test.mp3"	
+	}]
+}, {
+	"title": "title",
+	"artist": "artist",
+	"artwork": "artwork",
+	"tracks": [{
+		"title": "song title",
+		"source": "test.mp3"	
+	}, {
+		"title": "song title",
+		"source": "test.mp3"	
+	}, {
+		"title": "song title",
+		"source": "test.mp3"	
+	}]
+}, {
+	"title": "title",
+	"artist": "artist",
+	"artwork": "artwork",
+	"tracks": [{
+		"title": "song title",
+		"source": "test.mp3"	
+	}, {
+		"title": "song title",
+		"source": "test.mp3"	
+	}, {
+		"title": "song title",
+		"source": "test.mp3"	
+	}]
+}, {
+	"title": "title",
+	"artist": "artist",
+	"artwork": "artwork",
+	"tracks": [{
+		"title": "song title",
+		"source": "test.mp3"	
+	}, {
+		"title": "song title",
+		"source": "test.mp3"	
+	}, {
+		"title": "song title",
+		"source": "test.mp3"	
+	}]
+}, {
+	"title": "title",
+	"artist": "artist",
+	"artwork": "artwork",
+	"tracks": [{
+		"title": "song title",
+		"source": "test.mp3"	
+	}, {
+		"title": "song title",
+		"source": "test.mp3"	
+	}, {
+		"title": "song title",
+		"source": "test.mp3"	
+	}]
+}, {
 	"title": "title",
 	"artist": "artist",
 	"artwork": "artwork",
